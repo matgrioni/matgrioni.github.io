8f4c752f611f1870256d1209172100a70ed9aab1
Matias Grioni <matgrioni@gmail.com>
2017-07-04 23:42:18 -0700

    Simplification of site.

--- js/layout.js
+++ js/layout.js
@@ -11,49 +11,6 @@ var Layout = function () {
         ) ? true : false);
     }
 
-    // handle on page scroll
-    var handleHeaderOnScroll = function() {
-        if ($(window).scrollTop() > 60) {
-            $("body").addClass("page-on-scroll");
-        } else {
-            $("body").removeClass("page-on-scroll");
-        }
-    }
-
-    // Handle Header
-    var handleOnePageHeader = function() {
-        // jQuery to collapse the navbar on scroll
-        if ($('.navbar').offset().top > 150) {
-            $('.navbar-fixed-top').addClass('top-nav-collapse');
-        }
-        $(window).scroll(function() {
-            if ($('.navbar').offset().top > 150) {
-                $('.navbar-fixed-top').addClass('top-nav-collapse');
-            } else {
-                $('.navbar-fixed-top').removeClass('top-nav-collapse');
-            }
-        });
-
-        var $offset = 0;
-        $offset = $(".navbar-fixed-top").height()-20;
-        
-        // jQuery for page scrolling feature - requires jQuery Easing plugin
-        $('.js_nav-item a').bind('click', function(event) {
-            var $position = $($(this).attr('href')).offset().top;
-            $('html, body').stop().animate({
-                scrollTop: $position - $offset
-            }, 600);
-            event.preventDefault();
-        });
-
-        var $scrollspy = $('body').scrollspy({target: '.navbar-fixed-top', offset: $offset+2});
-
-        // Collapse Navbar When It's Clickicked
-        $(window).scroll(function() {
-            $('.navbar-collapse.in').collapse('hide');
-        });
-    }
-
     // handle carousel
     var handleCarousel = function() {
         var $item = $('.carousel .item'); 
@@ -136,16 +93,9 @@ var Layout = function () {
     return {
         init: function () {
             // initial setup for fixed header
-            handleHeaderOnScroll();
-            handleOnePageHeader(); // initial header
             handleCarousel(); // initial setup for carousel
             handleHeight(); // initial setup for group element height
             handleWorkPopup(); // initial setup for group work popup
-            
-            // handle minimized header on page scroll
-            $(window).scroll(function() {
-                handleHeaderOnScroll();
-            });
         },
 
         // To get the correct viewport width based on  http://andylangton.co.uk/articles/javascript/get-viewport-size-javascript/
